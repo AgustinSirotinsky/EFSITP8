e1685c5985fb2866f30c264d435809282a54c7b8
Agustin Sirotinsky <agussiro06@gmail.com>
2023-08-07 08:32:54 -0300

    bruh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
 
 function App() {
+  const headers = {
+    'X-Meteum-API-Key': '68820936-c4bf-46a9-b538-f26b61469509'
+  };
+  useEffect(() => {
+  fetch('https://api.meteum.ai/v1/forecast?lat=52.37125&lon=4.89388', { headers })
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => console.log(err));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <h1>hello</h1>
+  </div>
   );
 }
 
