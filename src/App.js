dca1def956698de70aad9c33fa60427229315b66
Agustin Sirotinsky <agussiro06@gmail.com>
2023-08-07 09:00:37 -0300

    consdi

--- src/App.js
+++ src/App.js
@@ -3,17 +3,6 @@ import './App.css';
 import { useState,useEffect } from 'react';
 
 function App() {
-  const headers = {
-    'X-Meteum-API-Key': '68820936-c4bf-46a9-b538-f26b61469509'
-  };
-  useEffect(() => {
-  fetch('https://api.meteum.ai/v1/forecast?lat=52.37125&lon=4.89388', { headers })
-    .then(res => res.json())
-    .then(res => {
-      console.log(res)
-    })
-    .catch(err => console.log(err));
-  }, []);
   return (
     <div>
     <h1>hello</h1>
