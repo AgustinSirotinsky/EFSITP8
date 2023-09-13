a7c9555786db5565bd01cab8a9699ed9ff9fa4d9
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-13 17:13:17 -0300

    highschore inciializado hyuyuetrhofhaoj

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import {Helmet} from "react-helmet";
 
 //Bootsrap
 import { Navbar, Nav } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 
 //css
 import './App.css';
@@ -14,9 +12,11 @@ import './App.css';
 //Routes
 import Home from './components/routes/Home';
 import Quiz from './components/routes/Quiz';
+import HighScore from './components/routes/Highschore';
 
 //Other
 import casita from './casita.png'
+import highschore from './highschore.png'
 
 export default function App() {
   return (
@@ -29,10 +29,14 @@ export default function App() {
           <Navbar.Brand href="/">
             <img src={casita} width="50" height="50" alt="home"/>
           </Navbar.Brand>
+          <Navbar.Brand href="/highSchore">
+            <img src={highschore} width="75" height="50" alt="highschore"/>
+          </Navbar.Brand>
       </Navbar>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/quiz" element={<Quiz />}></Route>
+        <Route path="/highschore" element={<HighScore />}></Route>
       </Routes>
     </BrowserRouter>
     </div>
---
+++ src/components/routes/Highschore.jsx
@@ -0,0 +1,11 @@
+//Bootstrap
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Highscore() {
+    return(
+        <div className='Highschore' style={{color:"white"}}>
+            <h1>HighScore</h1>
+        </div>
+    )
+}
